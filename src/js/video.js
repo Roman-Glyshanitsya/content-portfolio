ff010a79bb2b4f3bb2fc32a43b10a85e8473f832
Roman-Glyshanitsya <r.glyshanitsya@gmail.com>
2024-02-29 01:59:54 +0200

    projects video added

---
+++ src/js/video.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+  (function initPlayVideo() {
+    var videoCover = document.querySelector(".project__item__video-image");
+    var videoPlayerIframe = document.querySelector(
+      ".project__item__video-player iframe"
+    );
+
+    videoCover.addEventListener("click", function () {
+      videoCover.style.display = "none";
+      videoPlayerIframe.src += "&autoplay=1";
+    });
+  })();
+});
