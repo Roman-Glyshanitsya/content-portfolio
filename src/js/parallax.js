ac3850a2ca66de3a503e23dc5c1503cf1c9380c1
Roman-Glyshanitsya <r.glyshanitsya@gmail.com>
2024-03-04 02:43:13 +0200

    started parallax projects

---
+++ src/js/parallax.js
@@ -0,0 +1,9 @@
+// const heroSection = document.querySelector(".hero");
+// const servicesSection = document.querySelector(".services");
+const projectsList = document.querySelector(".projects");
+
+window.addEventListener("scroll", (e) => {
+  //   const scrollYFromSection =
+  //     window.screenY - (heroSection.offsetHeight + servicesSection.offsetHeight);
+  projectsList.style.cssText = `--scrollTop: ${scrollY - 1000}px`;
+});
--- src/main.js
+++ src/main.js
@@ -1,1 +1,2 @@
 import "./js/video";
+import "./js/parallax";
