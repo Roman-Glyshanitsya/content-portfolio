f3bd5a7fea687f2de83a9e522b3bdb05c14fb0ea
Roman-Glyshanitsya <r.glyshanitsya@gmail.com>
2024-04-18 00:45:27 +0300

    done projects

--- src/js/projects-parallax.js
+++ src/js/projects-parallax.js
@@ -1,5 +1,5 @@
 const projectsSection = document.querySelector(".projects");
 
 window.addEventListener("scroll", (e) => {
-  projectsSection.style.cssText = `--scrollTop: ${scrollY - 1000}px`;
+  projectsSection.style.cssText = `--scrollTop: ${scrollY - 1500}px`;
 });
