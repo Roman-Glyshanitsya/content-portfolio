91f682d8419dfec26cc757354b7dd4bece254539
Roman-Glyshanitsya <r.glyshanitsya@gmail.com>
2024-04-19 02:00:55 +0300

    update

--- src/js/video.js
+++ src/js/video.js
@@ -1,15 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
   (function initPlayVideo() {
     const videoCover = document.querySelectorAll(".projects__item__img");
-    const videoPlayerIframe = document.querySelector(
+    const videoPlayerIframe = document.querySelectorAll(
       ".pojects__item__thumb iframe"
     );
 
     console.log(videoCover);
     videoCover.forEach((item) => {
-      return item.addEventListener("click", function () {
+      item.addEventListener("click", function () {
         item.style.display = "none";
-        videoPlayerIframe.src += "&autoplay=1";
+        videoPlayerIframe.src = "&autoplay=1";
       });
     });
   })();
