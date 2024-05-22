51bd5653e7b9a7a3847fc519952931260cbe86da
Roman-Glyshanitsya <r.glyshanitsya@gmail.com>
2024-05-23 00:58:55 +0300

    update

---
+++ src/js/loader.js
@@ -0,0 +1,5 @@
+window.addEventListener("load", function () {
+  // hide loader after load page
+  var loader = document.querySelector(".loader");
+  loader.style.display = "none";
+});
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import "./js/loader";
 import "./js/video";
 import "./js/hero-parallax";
 import "./js/projects-parallax";
