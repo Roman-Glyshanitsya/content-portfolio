bc0e2498f5d747012b88ed468682fa8111baa694
Roman-Glyshanitsya <r.glyshanitsya@gmail.com>
2024-09-01 14:12:49 +0300

    update

--- src/js/projects-animation.js
+++ src/js/projects-animation.js
@@ -4,23 +4,40 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
 // onScroll
-gsap.from(".projects__item", {
+// gsap.from(".projects__item", {
+//   scrollTrigger: {
+//     trigger: ".projects",
+//     start: "-7% center",
+//     end: "bottom bottom",
+//     scrub: true,
+//     markers: true,
+//   },
+//   scale: 0.9,
+//   transformOrigin: "bottom center",
+//   stagger: 0.7,
+//   duration: 2,
+// });
+
+// img parallax ================================
+// const projectsSection = document.querySelector(".projects");
+
+// window.addEventListener("scroll", (e) => {
+//   projectsSection.style.cssText = `--scrollTop: ${scrollY - 1500}px`;
+// });
+gsap.to(".projects__up", {
   scrollTrigger: {
     trigger: ".projects",
-    start: "-7% center",
-    end: "bottom bottm",
     scrub: true,
-    // markers: true,
   },
-  scale: 0.9,
-  transformOrigin: "bottom center",
+  yPercent: -20,
   stagger: 0.7,
-  duration: 2,
 });
 
-// ================================
-const projectsSection = document.querySelector(".projects");
-
-window.addEventListener("scroll", (e) => {
-  projectsSection.style.cssText = `--scrollTop: ${scrollY - 1500}px`;
+gsap.to(".projects__down", {
+  scrollTrigger: {
+    trigger: ".projects",
+    scrub: true,
+  },
+  yPercent: 60,
+  stagger: 0.7,
 });
