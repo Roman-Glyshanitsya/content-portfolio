9c7409f57073c8498dbaa35329b47190ac5827df
Roman-Glyshanitsya <r.glyshanitsya@gmail.com>
2024-09-01 14:55:34 +0300

    update

--- src/js/projects-animation.js
+++ src/js/projects-animation.js
@@ -4,19 +4,19 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
 // onScroll
-// gsap.from(".projects__item", {
-//   scrollTrigger: {
-//     trigger: ".projects",
-//     start: "-7% center",
-//     end: "bottom bottom",
-//     scrub: true,
-//     markers: true,
-//   },
-//   scale: 0.9,
-//   transformOrigin: "bottom center",
-//   stagger: 0.7,
-//   duration: 2,
-// });
+gsap.from(".projects__item", {
+  scrollTrigger: {
+    trigger: ".projects",
+    start: "-7% center",
+    end: "bottom bottom",
+    scrub: true,
+    // markers: true,
+  },
+  scale: 0.7,
+  transformOrigin: "bottom center",
+  stagger: 0.7,
+  duration: 2,
+});
 
 // img parallax ================================
 // const projectsSection = document.querySelector(".projects");
@@ -24,20 +24,24 @@ gsap.registerPlugin(ScrollTrigger);
 // window.addEventListener("scroll", (e) => {
 //   projectsSection.style.cssText = `--scrollTop: ${scrollY - 1500}px`;
 // });
-gsap.to(".projects__up", {
-  scrollTrigger: {
-    trigger: ".projects",
-    scrub: true,
-  },
-  yPercent: -20,
-  stagger: 0.7,
-});
+// gsap.to(".projects__up", {
+//   scrollTrigger: {
+//     trigger: ".projects",
+//     start: "-7% center",
+//     end: "bottom bottom",
+//     scrub: true,
+//   },
+//   yPercent: -20,
+//   stagger: 0.7,
+// });
 
-gsap.to(".projects__down", {
-  scrollTrigger: {
-    trigger: ".projects",
-    scrub: true,
-  },
-  yPercent: 60,
-  stagger: 0.7,
-});
+// gsap.to(".projects__down", {
+//   scrollTrigger: {
+//     trigger: ".projects",
+//     start: "-7% center",
+//     end: "bottom bottom",
+//     scrub: true,
+//   },
+//   yPercent: 100,
+//   stagger: 0.7,
+// });
