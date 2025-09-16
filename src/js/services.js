2ea6aea5b4d829af4a20c6a49eb01aa7d12b9df5
Roman-Glyshanitsya <r.glyshanitsya@gmail.com>
2025-09-17 01:42:18 +0300

    up

---
+++ src/js/services.js
@@ -0,0 +1,19 @@
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from(".service__line", {
+  scrollTrigger: {
+    trigger: ".services",
+    markers: true,
+    start: "-10% center",
+    end: "-=200px",
+    scrub: true,
+  },
+  x: -500,
+  //   opacity: 0,
+  //   transformOrigin: "left center",
+  ease: "none",
+  stagger: 1,
+});
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
-import "./js/loader";
+// import "./js/loader";
 import "./js/video";
 import "./js/hero-parallax";
 import "./js/projects-animation";
 import "./js/open-contacts";
+import "./js/services";
 import "./js/clients";
